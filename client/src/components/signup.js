ca6188a73a4a597f71a6a20dbaeffd7db86cc6a8
Daniel <daniel.castelline@gmail.com>
2021-03-16 11:51:19 -0500

    "signup form"

--- client/src/components/signup.js
+++ client/src/components/signup.js
@@ -1,38 +1,39 @@
 import React from 'react';
 
-export function Signup(props) {
-    return(
-        <div className="signup-form">
-            <h3>Sign Up</h3>
-            <form className="form-horizontal">
-                <label className="email-label">Email</label>
-                <input className="email-input"
-                    type="text"
-                    id="email"
-                    name="email"
-                    placeholder="Email"
-                    value={this.state.email}
-                    onChange={this.handleChange}
-                />
+function SignupForm(props) {
+    return (
+        <form className="signup-form">
+            <div className="form-group">
                 <label className="username-label">Username</label>
                 <input className="username-input"
+                    name="username"
+                    value={props.signup}
+                    onChange={props.handleInputChange}
                     type="text"
                     id="username"
-                    name="username"
-                    placeholder="Username"
-                    value={this.state.username}
-                    onChange={this.handleChange}
+                    placeholder="Enter a username"
+                />
+                <label className="email-label">Email</label>
+                <input className="email-input"
+                    name="email"
+                    value={props.signup}
+                    onChange={props.handleInputChange}
+                    type="text"
+                    id="email"
+                    placeholder="Enter your email address"
                 />
                 <label className="password-label">Password</label>
                 <input className="password-input"
+                    name="password"
+                    value={props.signup}
+                    onChange={props.handleInputChange}
                     type="text"
                     id="password"
-                    name="password"
-                    placeholder="Password"
-                    value={this.state.password}
-                    onChange={this.handleChange}
+                    placeholder="Enter a password"
                 />
-            </form>
-        </div>
+            </div>
+        </form>
     )
-};
+}
+
+export default SignupForm;
