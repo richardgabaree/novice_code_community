37354f72cd6acafb99bbfe90bab689c5ae33639b
Richard Gabaree <richard.gabaree@gmail.com>
2021-03-16 12:56:10 -0500

    updated app.js

---
+++ client/src/utils/app.js
@@ -0,0 +1,16 @@
+import React from "react";
+import Login from "./pages/login.js";
+import Signup from "./pages/signup.js";
+
+
+function App() {
+    return (
+        <div>
+            <Header />
+            <Card />
+            <Section />
+        </div>
+    );
+}
+
+export default App;
